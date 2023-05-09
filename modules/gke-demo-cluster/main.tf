9dc4ed7024dc3df7e4ba39233e26467e9678cd36
Thomas Schuetz <thomas.schuetz@t-sc.eu>
2023-05-09 21:30:04 +0200

    feat: add k8sgpt
    Signed-off-by: Thomas Schuetz <thomas.schuetz@t-sc.eu>

--- modules/gke-demo-cluster/main.tf
+++ modules/gke-demo-cluster/main.tf
@@ -64,10 +64,7 @@ resource "google_container_node_pool" "primary_nodes" {
   cluster    = google_container_cluster.gke-demo-cluster.name
   node_count = var.node_count
   project    = var.project_id
-
-  labels  {
-    cluster_type = "gke-demo-cluster"
-  }
+  
 
   management {
     auto_upgrade = true
